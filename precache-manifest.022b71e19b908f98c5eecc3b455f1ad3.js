ec7a44e6e92e18d485f290a46100938a1f0975aa
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-07-30 04:46:07 +0530

    Updates

---
+++ precache-manifest.022b71e19b908f98c5eecc3b455f1ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554d71f4b10ba43abefd81aed4681953",
+    "url": "/cdtool1/index.html"
+  },
+  {
+    "revision": "8db94b66acb5686c375d",
+    "url": "/cdtool1/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2f9714c74aec3bdc312c",
+    "url": "/cdtool1/static/css/main.7d9c9697.chunk.css"
+  },
+  {
+    "revision": "8db94b66acb5686c375d",
+    "url": "/cdtool1/static/js/2.d4034329.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cdtool1/static/js/2.d4034329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9714c74aec3bdc312c",
+    "url": "/cdtool1/static/js/main.d271bc3a.chunk.js"
+  },
+  {
+    "revision": "c9a668d6fd84e7fd220d",
+    "url": "/cdtool1/static/js/runtime-main.614fb3d8.js"
+  }
+]);
